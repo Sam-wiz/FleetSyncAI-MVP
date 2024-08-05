b7c875aecc09370b8342c146677292e296531d0b
aman <amanm.ug23.cs@nitp.ac.in>
2024-08-06 02:35:57 +0530

    home page added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from '../public/truck.png'
+import { BiSolidUserAccount } from "react-icons/bi";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className={`navbar ${isOpen ? "active" : ""}`}>
+      <div className="logo">
+      <img src={logo} style={{ height: "60px",filter: "invert(1) grayscale(1) contrast(1)" }} alt="Logo" />
+        </div>
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#service">Services</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+        <li>
+  <a href="#register" style={{ fontSize: "34px" }}>
+    <BiSolidUserAccount />
+  </a>
+</li>
+      </ul>
+      <div className={`hamburger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
