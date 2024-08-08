6e90bb5fc0648402cc5436427a8e2c3b54e91477
aman <amanm.ug23.cs@nitp.ac.in>
2024-08-08 23:49:37 +0530

    dash board ,login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
-import Home from './pages/Home';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Management from './pages/Management.jsx';
+import Auth from './pages/Auth.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 function App() {
- 
-
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
+    return (
+        <Router>  {/* Ensure Router wraps the Routes component */}
+        <div>
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/management" element={<Management />} />
+            <Route path="/register" element={<Auth />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+           
+          </Routes>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
